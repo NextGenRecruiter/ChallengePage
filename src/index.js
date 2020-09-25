41f406afcb00fc954da12de793e7bc25d18f09c2
Misky Abshir <Misky.Abshir@gmail.com>
2020-09-25 16:12:40 -0500

    removed docker files

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`helloworld: listening on port ${port}`);
-});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
